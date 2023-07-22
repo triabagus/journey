eee80a052e1e8a20a1947e627304d4fc927b59ba
triabagus <triatop9@gmail.com>
2023-07-22 10:32:23 +0700

    Header & Nav: Styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,31 @@
 /*=============== SHOW MENU ===============*/
+const navMenu   = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose  = document.getElementById('nav-close');
 
+// Show menu
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// Hide Menu
+if(navClose){
+    navClose.addEventListener('click', () =>{
+       navMenu.classList.remove('show-menu') 
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink   = document.querySelectorAll('.nav__link')
 
+const linkAction = () =>{
+    const navmenu = document.getElementById('nav-menu')
+    // We when click on each nav__link, we remove the show menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR TO HEADER ===============*/
 
